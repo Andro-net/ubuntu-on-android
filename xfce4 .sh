5bd9d441b1602b2f04839aabc16fe8e35c0ab904
Andro-net <83906183+Andro-net@users.noreply.github.com>
2021-05-12 20:35:36 +0530

    Update xfce4 .sh

--- xfce4 .sh
+++ xfce4 .sh
@@ -8,13 +8,6 @@ apt install neofetch sudo -y
 apt install wget -y 
 apt install git -y
 apt install xfce4 -y
-clear
-echo " adding chrome browser , please wait"
-wget -q https://script.install.devinsideyou.com/google-chrome sudo chmod +x google-chrome && ./google-chrome
-ls
-chmod +x google-chrome 
-bash google chrome
-clear
 app install firefox
 clear
 vncserver 
