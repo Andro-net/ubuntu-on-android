d16e7dc99220dc78277d5bc3bdf15bfe56741619
Andro-net <83906183+Andro-net@users.noreply.github.com>
2021-05-22 00:58:54 +0530

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,7 +1,6 @@
 apt update && apt upgrade -y
 apt install sudo
-apt install xfce4-goodies -y
+apt install xfce4 xfce4-goodies -y
 apt install firefox -y
 apt install tigervnc-standalone-server -y
 vncserver 
-vncserver -kill
